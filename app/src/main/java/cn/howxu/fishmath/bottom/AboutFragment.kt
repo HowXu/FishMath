5edc99cb360cd428d8419c24aa2bfcef9bee42aa
HowXu <howxu366@outlook.com>
2024-07-02 12:45:39 +0800

    fix github activity

--- app/src/main/java/cn/howxu/fishmath/bottom/AboutFragment.kt
+++ app/src/main/java/cn/howxu/fishmath/bottom/AboutFragment.kt
@@ -10,6 +10,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageButton
 import androidx.constraintlayout.widget.ConstraintSet.Layout
+import cn.howxu.fishmath.Entry
 import cn.howxu.fishmath.R
 
 
@@ -20,17 +21,17 @@ class AboutFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
         return inflater.inflate(R.layout.fragment_about, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        //TODO 修不好URL跳转孩子
-        val github: View? = requireActivity().findViewById(R.id.github_page)
-        github!!.setOnClickListener{
-            val intent = Intent().setData(Uri.parse("baidu.com"))
-            requireContext().startActivity(intent)
+        // 修不好URL跳转孩子
+        val github: View = view.findViewById(R.id.github_page)
+        github.setOnClickListener{
+            //这里不能用 CATE.BROWSERABLE属性不然找不到Activtity处理
+            val intent = Intent(Intent.ACTION_VIEW).setData(Uri.parse("https://github.com/HowXu/FishMath"))    //务必要有https前缀不然找不到Activity
+            requireActivity().startActivity(intent)
         }
     }
 
